fd119d1dcd7e2424b1fafeeb40bd6f522a344ced
quocdai92 <quocdai92@gmail.com>
2013-09-24 19:33:00 +0700

    Update header.h

--- header.h
+++ header.h
@@ -2,10 +2,11 @@
 #include<stdio.h>
 using namespace std;
 #define IDMAXLEN 10
-#define NUMMAXLEN 5
-#define NUMKW 13
-#define NUMOP 12
-#define LENTOKEN 15
+#define NUMMAXLEN 6
+#define NUMOFIDENT
+#define NUMKW 14
+#define NUMOP 15
+#define LENTOKEN 20
 enum element{
 	t_null,			//0 null
 	t_ident,		//1 identifier
@@ -23,49 +24,49 @@ enum element{
 	t_geq,			//13 >=
 	t_lparen,		//14 (
 	t_rparen,		//15 )
-	t_div,			//16 %
-	t_comma,		//17 ,
-	t_semicolon,		//18 ;
-	t_becomes,		//19 :=
-	t_begin,		//20 begin	
-	t_end,			//21 end
-	t_if,			//22 if
-	t_then,			//23 then
-	t_while,		//24 while
-	t_do,			//25 do
-	t_call,			//26 call
-	t_const,		//27 const
-	t_var,			//28 var
-	t_program,		//29 program
-	t_procedure,		//30 procedure
-	t_space,		//31 space
-	t_for,			//32 for
-	t_else, 		//33 else
-	t_error,		//34 error
-	t_eof			//35 EOF
+	t_lbrave,		//16 [
+	t_rbrave,		//17 ]
+	t_div,			//18 %
+	t_comma,		//19 ,
+	t_semicolon,		//20 ;
+	t_assign,		//12 :=
+	t_begin,		//22 begin	
+	t_end,			//23 end
+	t_if,			//24 if
+	t_then,			//25 then
+	t_while,		//26 while
+	t_do,			//27 do
+	t_call,			//28 call
+	t_const,		//29 const
+	t_var,			//30 var
+	t_program,		//31 program
+	t_procedure,		//32 procedure
+	t_for,			//33 for
+	t_else, 		//34 else
+	t_error,		//35 error
+	t_eof,			//36 EOF
+	t_to,			//37 to
+	t_period		//38 .
 };
-char ch = ' ';		//last character read
+enum id_type{
+	id_constance,
+	id_keyword,
+	id_variable
+};
+char ch = ' ';			//last character read
 char id[IDMAXLEN+1];	//last identifier read
-int num;		//last numer read
+int num;				//last number read
 FILE* f;
 char* keyword[NUMKW+1] = {
 	"", "begin", "program", "end", "if", "then", "while",
 	"do", "call", "const", "var", "procedure", "for", "else"
 };
-int tkeyword[NUMKW+1] = {
-	t_null, t_begin, t_program, t_end, t_if, t_then, t_while,
-	t_do, t_call, t_const, t_var, t_procedure, t_for, t_else
-};
-char operation[NUMOP+1] = {
-	' ', '+', '-', '*', '/', '<', '>', '=', '(', ')', '%', ',', ';'
-};
-int toperation[NUMOP+1] = {
-	t_null, t_plus, t_minus, t_times, t_slash, t_les, t_gtr, t_equ,
-	t_lparen, t_rparen, t_div, t_comma, t_semicolon
-};
 typedef struct {
 	enum element tuto;
 	char name[LENTOKEN];
 	int value;
+	enum id_type type;
 }token;
 token mtoken;
+token listtoken[100];
+int numident = 0;
